3b30fa747489816056911cb34a78060a738636fa
HadiCyb <hadihabibi2000@gmail.com>
2019-02-20 22:04:05 +0400

    navigation

---
+++ App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Button, View, Text } from 'react-native';
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Signup')}
+        />
+      </View>
+    );
+  }
+}
+
+class Signup extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go to Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
+        />
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Login: {
+      screen: Login,
+    },
+    Signup: {
+      screen: Signup,
+    },
+  },
+  {
+    initialRouteName: 'Login',
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
